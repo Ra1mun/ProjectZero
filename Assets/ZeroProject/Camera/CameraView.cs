c3793e9988e4ad9901a10b17a4420f80e5cecc2d
Lost_Shadow <sobolm58@gmail.com>
2024-02-23 13:51:52 +0700

    add art design

--- Assets/ZeroProject/Camera/CameraView.cs
+++ Assets/ZeroProject/Camera/CameraView.cs
@@ -5,6 +5,6 @@ namespace ZeroProject.Camera
     public class CameraView : SceneObject.Realisation.SceneObject
     {
         public UnityEngine.Camera Camera => camera; 
-        [SerializeField] private UnityEngine.Camera camera;
+        [SerializeField] private new UnityEngine.Camera camera;
     }
 }
